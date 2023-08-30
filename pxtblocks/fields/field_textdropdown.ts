001b106c19d1609d7ca6b8a46aa69813aff95d04
jp-96 <jp-96@outlook.jp>
2023-08-30 14:39:38 +0900

    split text of "fieldOptions.values" to create options.

--- pxtblocks/fields/field_textdropdown.ts
+++ pxtblocks/fields/field_textdropdown.ts
@@ -8,9 +8,20 @@ namespace pxtblockly {
 
     export class FieldTextDropdown extends Blockly.FieldTextDropdown implements Blockly.FieldCustom {
         public isFieldCustom_ = true;
+        private menuGenerator_: any;
 
         constructor(text: string, options: FieldTextDropdownOptions, opt_validator?: Function) {
             super(text, options.values, opt_validator);
+        }
+
+        getOptions() {
+            let newOptions: string[][];
+            if (this.menuGenerator_) {
+                newOptions = this.menuGenerator_.split(",").map((s: string) => {
+                    return [s, s];
+                });
+            }
+            return newOptions;
         }
     }
 }
